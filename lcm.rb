ece8a9439b9f63da702b9b74e799cc189db05b3e
mankerious <marcomankerious@Marcos-MBP.home>
2016-03-31 10:16:06 -0400

    Code cleanup
    removed unnecessary connects

--- lcm.rb
+++ lcm.rb
@@ -4,26 +4,22 @@ require 'pp'
 require 'sinatra'
 require 'slim'
 
-# http://www.sitepoint.com/just-do-it-learn-sinatra-i/
-
+# reference: http://www.sitepoint.com/just-do-it-learn-sinatra-i/
 
 get '/' do 
 	@project_list=""
 	#get a list of projects for this user and display them
-	GoodData.with_connection() do |client|
-    	client.projects.each do |project|
-        	@project_list =  @project_list + project.title
-    	end
-	end
+	# GoodData.with_connection() do |client|
+ #    	client.projects.each do |project|
+ #        	@project_list =  @project_list + project.title
+ #    	end
+	# end
   slim :index 
 end
 
 
 post '/' do
 
-# Initialize variables
-  	
-  	# @project =  params[:project]
  	if params[:project].to_s == ''
  		@project = params[:projectid]
  	else
@@ -33,7 +29,6 @@ post '/' do
 	client = GoodData.connect()
 	project = client.projects(@project)
 	@project_title = project.title
-
 
   	slim :project
 end
