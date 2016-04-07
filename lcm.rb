f0a34475b29ba5bd34491da78070f248d9ba56c4
mankerious <marcomankerious@Marcos-MBP.home>
2016-04-07 14:51:21 -0400

    clone master project

--- lcm.rb
+++ lcm.rb
@@ -22,17 +22,29 @@ end
 post '/project' do
 
  	if params[:project].to_s == ''
- 		@project_pid = params[:projectid]
+ 		$project_pid = params[:projectid]
  	else
- 		@project_pid = params[:project]
+ 		$project_pid = params[:project]
 	end
 
 	client = GoodData.connect('mustang@gooddata.com', 'jindrisska', server: 'https://mustangs.intgdc.com', verify_ssl: false )
-	project = client.projects(@project_pid)
+	project = client.projects($project_pid)
 	@project_title = project.title
-  @customer_name = params[:customer_name]
+  $customer_name = params[:customer_name]
 
 
   	slim :project
 end
 
+post '/clone' do
+
+     client = GoodData.connect('mustang@gooddata.com', 'jindrisska', server: 'https://mustangs.intgdc.com', verify_ssl: false )
+     project = client.projects($project_pid)
+     project= project.clone(
+          :title => "#{$customer_name} Master - #{project.title}",
+          :with_data => true,
+          :auth_token => 'mustangs'
+        )     
+
+    slim :clone
+end
