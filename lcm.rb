33e1b9167ce6094787015bb2f192c31c31e6636c
mankerious <nynjct@Marco-Mankeriouss-MacBook-Pro.local>
2016-04-18 17:09:16 -0400

    Update brick locations

--- lcm.rb
+++ lcm.rb
@@ -54,35 +54,35 @@ post '/index' do
         })
         load_schedule.disable!
 
-        # filters_process = redeploy_or_create_process(@master_project, 'appstore://user_filters_brick', {})
-        # filters_schedule = redeploy_or_create_schedule(filters_process, load_schedule, 'main.rb', {
-        #   name: 'filters',
-        #   params: {
-        #     input_source: "filters.csv",
-        #     sync_mode: "sync_one_project_based_on_custom_id",
-        #     organization: DOMAIN,
-        #     CLIENT_GDC_PROTOCOL: 'https',
-        #     CLIENT_GDC_HOSTNAME: HOSTNAME,
-        #     filters_config: {
-        #       user_column: "login",
-        #       labels: [{label: "label.departments.name", "column": "department"}]
-        #     }
-        #   }
-        # })
-        # filters_schedule.disable!
+        filters_process = redeploy_or_create_process(@master_project, 'https://github.com/gooddata/app_store/tree/master/apps/user_filters_brick', {})
+        filters_schedule = redeploy_or_create_schedule(filters_process, load_schedule, 'main.rb', {
+          name: 'filters',
+          params: {
+            input_source: "filters.csv",
+            sync_mode: "sync_one_project_based_on_custom_id",
+            organization: DOMAIN,
+            CLIENT_GDC_PROTOCOL: 'https',
+            CLIENT_GDC_HOSTNAME: HOSTNAME,
+            filters_config: {
+              user_column: "login",
+              labels: [{label: "label.departments.name", "column": "department"}]
+            }
+          }
+        })
+        filters_schedule.disable!
 
-        # add_users_process = redeploy_or_create_process(@master_project, 'appstore://users_brick', {})
-        # add_users_schedule = redeploy_or_create_schedule(add_users_process, filters_schedule, 'main.rb', {
-        #   name: 'users',
-        #   params: {
-        #     input_source: "users.csv",
-        #     sync_mode: "sync_one_project_based_on_custom_id",
-        #     organization: DOMAIN,
-        #     CLIENT_GDC_PROTOCOL: 'https',
-        #     CLIENT_GDC_HOSTNAME: HOSTNAME
-        #   }
-        # })
-        # add_users_schedule.disable!
+        add_users_process = redeploy_or_create_process(@master_project, 'https://github.com/gooddata/app_store/tree/master/apps/users_brick', {})
+        add_users_schedule = redeploy_or_create_schedule(add_users_process, filters_schedule, 'main.rb', {
+          name: 'users',
+          params: {
+            input_source: "users.csv",
+            sync_mode: "sync_one_project_based_on_custom_id",
+            organization: DOMAIN,
+            CLIENT_GDC_PROTOCOL: 'https',
+            CLIENT_GDC_HOSTNAME: HOSTNAME
+          }
+        })
+        add_users_schedule.disable!
 
         @service_segment = create_or_get_segment(@domain, params[:segment_name], @master_project, version: VERSION)
 
@@ -95,7 +95,7 @@ post '/index' do
       ###########
       # RELEASE #
       ###########
-      #fix errors, can it causes 4.5 min
+
       # @domain.synchronize_clients
       # @domain.provision_client_projects
 
@@ -179,7 +179,7 @@ end
 post '/provision_clients' do
   
    @version='1.0.0'
-   @client = GoodData.connect('mustang@gooddata.com', 'jindrisska', server: 'https://mustangs.intgdc.com', verify_ssl: false )
+   @client = GoodData.connect(LOGIN, PASSWORD, server: FQDN, verify_ssl: false )
    @domain=@client.domain('mustangs')
    @segment = create_or_get_segment(@domain, params[:segment_id1], @master_project, version: @version)
    create_or_get_client(@segment, params[:client_name1])
@@ -206,58 +206,9 @@ end
 post '/settings' do
 
 
-  # client = GoodData.connect('mustang@gooddata.com', 'jindrisska', server: 'https://mustangs.intgdc.com', verify_ssl: false )
-  # project = client.projects(@project_pid)
-  # @customer_name = params[:customer_name]
-  # project= project.clone(
-  #         :title => "#{@customer_name} Master",
-  #         :with_data => true,
-  #         :auth_token => 'mustangs'
-
-  #       )     
-  # @project_title = project.title
 
   slim :index
 end
 
-
-post '/project' do
-
-  	slim :project
-end
-
-
-
-
-post '/segment_details' do
-   
-   @version='1.0.0'
-   client = GoodData.connect('mustang@gooddata.com', 'jindrisska', server: 'https://mustangs.intgdc.com', verify_ssl: false )
-   @domain=client.domain('mustangs')
-   # master_project = client.projects($project_pid)
-   # @domain.remove_segment($segment_name)
-   pp $segment.id
-   create_or_get_client($segment, "acme_client1")
-   @domain.synchronize_clients
-   @domain.provision_client_projects
-
-    slim :segment_details
-end
-
-
-
-
-post '/segments' do
-   
-   @version='1.0.0'
-   client = GoodData.connect('mustang@gooddata.com', 'jindrisska', server: 'https://mustangs.intgdc.com', verify_ssl: false )
-   @domain=client.domain('mustangs')
-   basic_master_project = client.projects(@project_pid)
-   $segment_name=params[:segment_name]
-   @service_segment = create_or_get_segment(@domain, $segment_name, basic_master_project, version: @version)
-   $segment_id = @service_segment.id
-
-    slim :segment_details
-end
 
 
